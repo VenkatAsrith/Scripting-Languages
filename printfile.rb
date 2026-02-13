faed48688a06b6ac4861801f88739aaf2923d3a8
VenkatAsrith <venkatasrith29@gmail.com>
2026-02-13 10:43:01 +0530

    Add filename input and extension display
    Prompt user for a filename and display its extension.

--- printfile.rb
+++ printfile.rb
@@ -1,1 +1,13 @@
 
+print "Enter filename: "
+filename = gets.chomp
+
+extension = File.extname(filename)
+
+puts "File extension: #{extension}"
+
+
+
+output :
+  Enter filename: python.py
+File extension: .py
