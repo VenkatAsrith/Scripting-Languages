c2cbb2cb2fa1a31d01415b9d1ac350a589a46b4a
VenkatAsrith <venkatasrith29@gmail.com>
2026-02-13 10:40:46 +0530

    Add script to reverse first and last names

---
+++ Reverse.rb
@@ -0,0 +1,14 @@
+print "Enter first name: "
+first = gets.chomp
+
+print "Enter last name: "
+last = gets.chomp
+
+puts "Reversed Name: #{last} #{first}"
+
+
+output
+
+Enter first name: venkat asrith
+Enter last name: konam 
+Reversed Name: konam  venkat asrith
