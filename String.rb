b0e8b422afc5b97a0e5a7915be1854601fe2e586
VenkatAsrith <venkatasrith29@gmail.com>
2026-02-13 10:37:58 +0530

    Implement string repetition based on user input

---
+++ String.rb
@@ -0,0 +1,18 @@
+
+
+print "Enter a string: "
+str = gets.chomp
+
+print "Enter a non-negative integer: "
+n = gets.chomp.to_i
+
+result = str * n
+
+puts "Result: #{result}"
+
+
+Output :
+
+Enter a string: venkat
+Enter a non-negative integer: 5
+Result: venkatvenkatvenkatvenkatvenkat
